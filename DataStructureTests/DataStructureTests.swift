17e2948d1afdb1ff0b1a26d8981685e6b721e3e8
Juwon-Yun <eooll2@daum.net>
2023-01-16 23:39:48 +0900

    - update: QueueTest.swift

--- DataStructureTests/DataStructureTests.swift
+++ DataStructureTests/DataStructureTests.swift
@@ -17,4 +17,24 @@ class QueueTest: XCTestCase{
         XCTAssertEqual(queue.front, nil)
         XCTAssertNil(queue.dequeue())
     }
+    
+    func testOneElement() -> Void{
+        var queue  = Queue<Int>([])
+        queue.enqueue(1)
+        
+        XCTAssertFalse(queue.isEmpty)
+        XCTAssertEqual(queue.count, 1)
+        XCTAssertEqual(queue.dequeue(), 1)
+        XCTAssertEqual(queue.front, nil)
+        XCTAssertEqual(queue.count, 0)
+        
+        queue.enqueue(2)
+        
+        let result = queue.dequeue()
+        
+        XCTAssertEqual(result , 2)
+        XCTAssertTrue(queue.isEmpty)
+        XCTAssertEqual(queue.count, 0)
+        XCTAssertEqual(queue.front, nil)
+    }
 }
