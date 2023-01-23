6a8dac819ebf8e8ac0a1a15c9fdd012f5ca5424b
Juwon-Yun <eooll2@daum.net>
2023-01-23 16:08:14 +0900

    - update: verifyMaxHeap test

--- DataStructureTests/Heap/HeapUnitTest.swift
+++ DataStructureTests/Heap/HeapUnitTest.swift
@@ -22,18 +22,4 @@ class HeapTest : XCTestCase{
         }
         return true
     }
-    
-    fileprivate func verifyMinHeap(_ heap : Heap<Int>) -> Bool{
-        for i in 0..<heap.nodes.count{
-            let left = heap.leftChildIndex(ofIndex: i)
-            let right = heap.rightChildIndex(ofIndex: i)
-            let parent = heap.parentIndex(ofIndex: i)
-            
-            if left < heap.nodes.count && heap.nodes[i] > heap.nodes[left] {return false}
-            if right < heap.nodes.count && heap.nodes[i] > heap.nodes[right] {return false}
-            if i > 0 && heap.nodes[parent] > heap.nodes[i] {return false}
-        }
-        
-        return true
-    }
 }
